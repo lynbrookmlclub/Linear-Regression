736d30b4c951425fe0217c93c0644c66495ce188
Patrick <huangpatrick16777216@gmail.com>
2021-10-16 14:50:17 -0700

    Update lin_reg.py

--- lin_reg.py
+++ lin_reg.py
@@ -1,49 +1,33 @@
-"""
-install: 
-    - sealion
-    - matplotlib
-    - numpy 
-"""
-
-
 import numpy as np
 
 # Y = 2x + 1
 
 np.random.seed(420)
 
-X = np.random.randn(100,1) # 100 random samples
-
+X = np.random.randn(100, 1)  # Array of 100 rows and 1 column. Random samples
 y = 2 * X + 1
 
-import matplotlib.pyplot as plt
-
-#plt.scatter(X, y, color = "green")
-#plt.show()
+# random offsets
+X += np.random.randn(100, 1) / 10
+y += np.random.randn(100, 1) / 10
 
 # use a framework to find the line y = mx + b that makes X into Y
+# remember to star SeaLion: https://github.com/anish-lakkapragada/SeaLion
 
 from sealion.regression import LinearRegression
 
 lin_reg = LinearRegression()
+lin_reg.fit(X, y.flatten())   # run the guessing game 
 
+# what were the m and b values that were learnt?
+# m is lin_reg.weights[0] and b is lin_reg.bias
 
-lin_reg.fit(X, y.flatten()) # run the guessing game 
+print(lin_reg.weights[0], lin_reg.bias)
 
-# what were the m and b values that were learnt? The
+# Display line and points
+import matplotlib.pyplot as plt
 
-print(lin_reg.weights, lin_reg.bias)
-
-
-plt.plot(X, y, label = "actual data")
+plt.scatter(X, y, label = "actual data")
 plt.plot(X, lin_reg.predict(X), label = "predicted data")
 plt.legend()
 plt.show()
-
-# pip install sealion
-
-
-
-
-
-
